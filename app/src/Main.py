b42c50118a98bb36dc873fff30269c6b61c42745
Grégory Ployart <gregory.ployart@gmail.com>
2022-02-12 00:10:56 +0100

    Icons added + OpenGL frame placed

--- app/src/Main.py
+++
@@ -1,9 +0,0 @@
-# This script is the main function of this application.
-
-
-def main():
-    print("Main function launched!")
-
-
-if __name__ == '__main__':
-    main()
---
+++ src/app/Main.py
@@ -0,0 +1,20 @@
+# This script is the main function of this application.
+
+
+def main():
+    print("Main function launched!")
+    """
+    src = QtWidgets.QApplication(sys.argv)
+    # create window
+    window = QtWidgets.QMainWindow()
+
+    ui = UiMainWindow(window)
+    # fill window
+    # ui.setupUi(window)
+
+    window.show()
+    sys.exit(src.exec_())
+    """
+
+if __name__ == '__main__':
+    main()
